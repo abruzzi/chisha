4b7db5976783750d9d66fe925a23433d31dcb5bf
Qiu Juntao <juntao.qiu@gmail.com>
2014-09-18 22:35:24 +1000

    fix the encoding bug when launch by crontab

--- lunch.rb
+++ lunch.rb
@@ -1,4 +1,4 @@
-# encoding: utf-8
+# encoding: UTF-8
 require 'json'
 
 def first
--- lunch.sh
+++ lunch.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-chisha=`ruby $HOME/develop/ruby/chisha/lunch.rb`
+export LC_ALL=en_US.UTF-8  
+export LANG=en_US.UTF-8
+
+cd $HOME/develop/ruby/chisha
+chisha=`ruby lunch.rb`
 osascript -e "display notification \"${chisha}\" with title \"Chisha?\""
